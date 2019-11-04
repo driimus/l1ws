6d3ce6abdbb390eadfdf8f19c18e531a3c1a59f8
petrec <petrec@coventry.ac.uk>
2019-11-04 16:43:31 +0000

    REFACTOR Wrapped uploadPicture() in try-catch block.

--- modules/article/upload-picture.js
+++ modules/article/upload-picture.js
@@ -6,11 +6,15 @@ const mime = require('mime-types')
 const path = require('path')
 
 const uploadPicture = async(img) => {
-	if(img.type.split('/')[0] !== 'image') throw new Error('invalid image MIME type')
-	const extension = mime.extension(img.type)
-	const picPath = `public/uploads/${path.basename(img.path)}.${extension}`
-	await fs.copy(img.path, picPath)
-	return picPath
+	try {
+		if(img.type.split('/')[0] !== 'image') throw new Error('invalid image MIME type')
+		const extension = mime.extension(img.type)
+		const picPath = `public/uploads/${path.basename(img.path)}.${extension}`
+		await fs.copy(img.path, picPath)
+		return picPath
+	} catch(err) {
+		throw err
+	}
 }
 
 module.exports = Article => Article.prototype.uploadPicture = uploadPicture
