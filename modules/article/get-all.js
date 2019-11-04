560d34d5101b8390c8cb0861dcea17d07ab31c0c
petrec <petrec@coventry.ac.uk>
2019-11-04 17:31:20 +0000

    GREEN Throw an error if there are no published articles.
    * Pass 'error if no published articles' test.

--- modules/article/get-all.js
+++ modules/article/get-all.js
@@ -11,6 +11,7 @@ const getAll = async function() {
 		// TO-DO: implement pagination
 		const sql = 'SELECT * FROM article ORDER BY created_at DESC'
 		const {rows: articles} = await this.db.query(sql)
+		if(articles.length === 0) throw new Error('found no published articles')
 		return articles
 	} catch(err) {
 		throw err
