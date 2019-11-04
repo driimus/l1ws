dc635debd161c6758cb0c994ef8ed11820af81bf
petrec <petrec@coventry.ac.uk>
2019-11-04 12:00:23 +0000

    Added node-postgres package.

--- modules/user.js
+++ modules/user.js
@@ -2,9 +2,8 @@
 'use strict'
 
 const bcrypt = require('bcrypt-promise')
-// const fs = require('fs-extra')
+const fs = require('fs-extra')
 const mime = require('mime-types')
-const sqlite = require('sqlite-async')
 const saltRounds = 10
 
 module.exports = class User {
