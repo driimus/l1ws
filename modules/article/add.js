2a09ff0867f45299f301b6765acda981cfe4eab3
petrec <petrec@coventry.ac.uk>
2019-11-04 16:06:12 +0000

    GREEN Added dedicated article schema validation.
    * Pass 'error if blank article summary' test.
    * Pass 'error if blank article thumbnail' test.
    * Pass 'error if blank article content' test.

--- modules/article/add.js
+++ modules/article/add.js
@@ -1,10 +1,22 @@
 
 'use strict'
 
+const isValid = async(article) => {
+	try {
+		const required = ['headline', 'summary', 'thumbnail', 'content']
+		required.forEach(section => {
+			if(!article.hasOwnProperty(section) || article[section].length === 0)
+				throw new Error(`missing article ${section}`)
+		})
+		return true
+	} catch(err) {
+		throw err
+	}
+}
+
 const add = async function(userId, article) {
 	try {
-		if(!article.hasOwnProperty('headline') || article.headline.length === 0)
-			throw new Error('missing article headline')
+		await isValid(article)
 		if(userId === undefined) throw new Error(`missing user ID ${userId}`)
 		const sql = 'INSERT INTO article(author_id, data) VALUES($1, $2)'
 		await this.db.query(sql, [userId, article])
