d5bee0f866fa8bae8170cb535b2be860456ea7b3
petrec <petrec@coventry.ac.uk>
2019-11-04 16:36:48 +0000

    REFACTOR Wrapped function in try-catch block.

--- modules/article/get-all.js
+++ modules/article/get-all.js
@@ -1,11 +1,20 @@
 
 'use strict'
 
+/**
+ * Retrieves all the stored articles in reverse chronological order.
+ *
+ * @async
+ */
 const getAll = async function() {
-	// TO-DO: implement pagination
-	const sql = 'SELECT * FROM article ORDER BY created_at DESC'
-	const {rows} = await this.db.query(sql)
-	return rows
+	try {
+		// TO-DO: implement pagination
+		const sql = 'SELECT * FROM article ORDER BY created_at DESC'
+		const {rows: articles} = await this.db.query(sql)
+		return articles
+	} catch(err) {
+		throw err
+	}
 }
 
 module.exports = Article => Article.prototype.getAll = getAll
