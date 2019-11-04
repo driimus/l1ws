31cdf82a5a6f9deacc87033db3704073268b4534
petrec <petrec@coventry.ac.uk>
2019-11-04 15:47:54 +0000

    GREEN Added article schema validation.
    * Pass 'error if blank article headline' test.

--- modules/article/add.js
+++ modules/article/add.js
@@ -2,6 +2,8 @@
 'use strict'
 
 const add = async function(userId, article) {
+	if(!article.hasOwnProperty('headline') || article.headline.length === 0)
+		throw new Error('missing article headline')
 	if(userId === undefined) throw new Error(`missing user ID ${userId}`)
 	const sql = 'INSERT INTO article(author_id, data) VALUES($1, $2)'
 	await this.db.query(sql, [userId, article])
