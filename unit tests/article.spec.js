2693fe1908edbc41763218bf9af5db75e41f1c12
petrec <petrec@coventry.ac.uk>
2019-11-04 15:55:44 +0000

    RED Articles should have a thumbnail, summary content.

--- unit tests/article.spec.js
+++ unit tests/article.spec.js
@@ -3,6 +3,14 @@
 
 const Articles = require('../modules/article')
 
+const dummy = {
+	headline: 'Test article title',
+	summary: 'Test article summary that is reasonably short',
+	thumbnail: 'mockdir/fixtures/some.png',
+	content: `Test article body. All the multi-line content
+	goes here, with no minimum character count.`
+}
+
 beforeEach(async done => {
 	this.article = await new Articles()
 	done()
@@ -17,27 +25,13 @@ describe('add()', () => {
 
 	test('add a valid article', async done => {
 		expect.assertions(1)
-		const uId = 1
-		const added = await this.article.add(uId, {
-			headline: 'Test article title',
-			summary: 'Test article summary that is reasonably short',
-			thumbnail: 'mockdir/fixtures/some.png',
-			content: `Test article body. All the multi-line content
-			goes here, with no minimum character count.`
-		})
+		const added = await this.article.add(1, dummy)
 		expect(added).toBe(true)
 		done()
 	})
 
 	test('error if missing userId', async done => {
 		expect.assertions(1)
-		const dummy = {
-			headline: 'Test article title',
-			summary: 'Test article summary that is reasonably short',
-			thumbnail: 'mockdir/fixtures/some.png',
-			content: `Test article body. All the multi-line content
-			goes here, with no minimum character count.`
-		}
 		const uId = undefined
 		await expect( this.article.add(uId, dummy) )
 			.rejects.toEqual( Error(`missing user ID ${uId}`) )
@@ -46,15 +40,37 @@ describe('add()', () => {
 
 	test('error if blank article headline', async done => {
 		expect.assertions(1)
-		const dummy = {
-			// no headline
-			summary: 'Test article summary that is reasonably short',
-			thumbnail: 'mockdir/fixtures/some.png',
-			content: `Test article body. All the multi-line content
-			goes here, with no minimum character count.`
-		}
-		await expect( this.article.add(1, dummy) )
+		// Article object with no headline.
+		const {summary, thumbnail, content} = dummy
+		await expect( this.article.add(1, {summary, thumbnail, content, headline: ''}) )
 			.rejects.toEqual( Error('missing article headline') )
+		done()
+	})
+
+	test('error if blank article summary', async done => {
+		expect.assertions(1)
+		// Article object with no summary.
+		const { headline, thumbnail, content } = dummy
+		await expect( this.article.add(1, {headline, thumbnail, content, summary: ''}) )
+			.rejects.toEqual( Error('missing article summary') )
+		done()
+	})
+
+	test('error if blank article thumbnail', async done => {
+		expect.assertions(1)
+		// Article object with no summary.
+		const { headline, summary, content } = dummy
+		await expect( this.article.add(1, {headline, summary, content, thumbnail: ''}) )
+			.rejects.toEqual( Error('missing article thumbnail') )
+		done()
+	})
+
+	test('error if blank article content', async done => {
+		expect.assertions(1)
+		// Article object with no content.
+		const { headline, summary, thumbnail } = dummy
+		await expect( this.article.add(1, {headline, summary, thumbnail, content: ''}) )
+			.rejects.toEqual( Error('missing article content') )
 		done()
 	})
 
