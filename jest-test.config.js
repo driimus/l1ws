2c179b38ee65d047de41df2546f3bb496b2b5ab7
petrec <petrec@coventry.ac.uk>
2019-11-04 12:48:50 +0000

    Set test coverage threshold to 100%.

--- jest-test.config.js
+++ jest-test.config.js
@@ -1,3 +1,4 @@
+
 'use strict'
 
 module.exports = {
@@ -6,10 +7,10 @@ module.exports = {
 	collectCoverage: true,
 	coverageThreshold: {
 		global: {
-			branches: 0,
-			functions: 0,
-			lines: 0,
-			statements: 0
+			branches: 100,
+			functions: 100,
+			lines: 100,
+			statements: 100
 		}
 	},
 	testPathIgnorePatterns: [
