0208309e88797a0fea94d349caf8c118ddd6a924
petrec <petrec@coventry.ac.uk>
2019-11-04 16:42:17 +0000

    GREEN Added uploaded file type validation.
    * Pass 'error if file is not an image' test.

--- modules/article/upload-picture.js
+++ modules/article/upload-picture.js
@@ -6,6 +6,7 @@ const mime = require('mime-types')
 const path = require('path')
 
 const uploadPicture = async(img) => {
+	if(img.type.split('/')[0] !== 'image') throw new Error('invalid image MIME type')
 	const extension = mime.extension(img.type)
 	const picPath = `public/uploads/${path.basename(img.path)}.${extension}`
 	await fs.copy(img.path, picPath)
