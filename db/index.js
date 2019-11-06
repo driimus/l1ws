53485cbbf380d8281bd857deb79e334adba8ea96
petrec <petrec@coventry.ac.uk>
2019-11-06 15:43:01 +0000

    Documented the database wrapper.

--- db/index.js
+++ db/index.js
@@ -5,8 +5,16 @@ const { Pool } = require('pg')
 
 const config = require('./config.js')
 
+/**
+ * Class representing a pool of database connections.
+ * @extends Pool
+ */
 module.exports = class Database extends Pool {
 
+	/**
+   * Create a new Pool.
+   * @param {string} [env=*Node environment*] - The database environment to be used.
+   */
 	constructor(env=process.env.NODE_ENV) {
 		return super(config[env])
 	}
