def9105074ae3338a46e861025708c63741d9a39
petrec <petrec@coventry.ac.uk>
2019-11-07 17:45:46 +0000

    REFACTOR Wrapped setAdmin() in try-catch block.

--- modules/user/set-admin.js
+++ modules/user/set-admin.js
@@ -11,11 +11,17 @@
  * @returns {boolean} Whether the status was successfully changed.
  */
 const setAdmin = async function(admin, username, toAdmin=true) {
-	const byAdmin = await this.isAdmin(admin)
-	if (!byAdmin) throw new Error(`user "${admin}" is not an admin`)
-	const sql = 'UPDATE users SET is_admin=$2 WHERE username=$1'
-	await this.db.query(sql, [username, toAdmin])
-	return true
+	try {
+		// Check that the request was made by an admin.
+		const byAdmin = await this.isAdmin(admin)
+		if (!byAdmin) throw new Error(`user "${admin}" is not an admin`)
+		// Update user status.
+		const sql = 'UPDATE users SET is_admin=$2 WHERE username=$1'
+		await this.db.query(sql, [username, toAdmin])
+		return true
+	} catch(err) {
+		throw err
+	}
 }
 
 module.exports = User => User.prototype.setAdmin = setAdmin
