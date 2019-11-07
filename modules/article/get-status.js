ad9a9d0c607f2cd0777d53673907d4c155ab9730
petrec <petrec@coventry.ac.uk>
2019-11-07 14:53:44 +0000

    GREEN Check whether article exists in status getter.
    * Pass 'error if article does not exist' test.

--- modules/article/get-status.js
+++ modules/article/get-status.js
@@ -11,6 +11,7 @@
 const getStatus = async function(id) {
 	const sql = 'SELECT status FROM article WHERE id=$1'
 	const {rows: [article] } = await this.db.query(sql, [id])
+	if(article === undefined) throw new Error(`article with ID "${id}" not found`)
 	return article.status
 }
 
