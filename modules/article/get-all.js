7cd3a436ff3abe208b62248436ddd3352ac6289b
petrec <petrec@coventry.ac.uk>
2019-11-07 17:05:14 +0000

    GREEN Added status filter for article getter.
    * Pass 'get approved articles in reverse chronological order' test.

--- modules/article/get-all.js
+++ modules/article/get-all.js
@@ -11,7 +11,7 @@
 const getAll = async function(showHidden=false) {
 	try {
 		// TO-DO: implement pagination
-		const sql = 'SELECT * FROM article ORDER BY created_at DESC'
+		const sql = `SELECT * FROM article ${showHidden === false ? 'WHERE status=\'approved\'' : ''} ORDER BY created_at DESC`
 		const {rows: articles} = await this.db.query(sql)
 		if(articles.length === 0) throw new Error('found no published articles')
 		return articles
