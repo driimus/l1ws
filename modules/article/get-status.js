48692919069587c898c3de725f0cdf127354563a
petrec <petrec@coventry.ac.uk>
2019-11-07 15:09:00 +0000

    GREEN Add article ID validation.
    * Pass 'error if article ID is not numeric' test.

--- modules/article/get-status.js
+++ modules/article/get-status.js
@@ -9,6 +9,8 @@
  * @returns {string} Current submission status (approved/pending).
  */
 const getStatus = async function(id) {
+	id = parseInt(id)
+	if (isNaN(id)) throw new Error('invalid article ID')
 	const sql = 'SELECT status FROM article WHERE id=$1'
 	const {rows: [article] } = await this.db.query(sql, [id])
 	if(article === undefined) throw new Error(`article with ID "${id}" not found`)
