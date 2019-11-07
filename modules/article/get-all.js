2dfb0a636eea34afb4ebb533f9f95d5fd7864553
petrec <petrec@coventry.ac.uk>
2019-11-07 17:07:46 +0000

    REFACTOR Changed SQL query formatting and updated docs.

--- modules/article/get-all.js
+++ modules/article/get-all.js
@@ -6,12 +6,15 @@
  *
  * @param {boolean} showHidden - Flag for filtering articles by status.
  * @async
- * @returns {Array} List of the published articles as objects.
+ * @returns {Array} List of the requested articles as objects.
  */
 const getAll = async function(showHidden=false) {
 	try {
 		// TO-DO: implement pagination
-		const sql = `SELECT * FROM article ${showHidden === false ? 'WHERE status=\'approved\'' : ''} ORDER BY created_at DESC`
+		const sql = `SELECT * FROM article
+			${showHidden === false ? 'WHERE status=\'approved\'' : ''}
+			ORDER BY created_at DESC
+		`
 		const {rows: articles} = await this.db.query(sql)
 		if(articles.length === 0) throw new Error('found no published articles')
 		return articles
