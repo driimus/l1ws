1dac4b7ac9656b0007ed5f560dcdd5ee1a8fc73a
petrec <petrec@coventry.ac.uk>
2019-11-07 16:14:51 +0000

    GREEN Added status filter in individual article getter.
    * Pass 'get hidden article with existing ID' test.

--- modules/article/get.js
+++ modules/article/get.js
@@ -5,14 +5,17 @@
  * Retrieves all the stored articles in reverse chronological order.
  *
  * @param {number} id - The ID of the requested article.
+ * @param {boolean} showHidden - Flag for filtering articles by status.
  * @async
  * @returns {object} Article with a matching id.
  */
-const get = async function(id) {
+const get = async function(id, showHidden=false) {
 	try {
 		// Check that Article ID is numeric.
 		if (isNaN(id)) throw new Error('invalid article ID')
-		const sql = 'SELECT * FROM article WHERE id=$1'
+		const sql = `SELECT * FROM article WHERE id=$1
+			${showHidden === false ? 'AND status=\'approved\'' : ''}
+		`
 		// Get first result.
 		const {rows: [article]} = await this.db.query(sql, [id])
 		if(article === undefined) throw new Error(`article with ID "${id}" not found`)
