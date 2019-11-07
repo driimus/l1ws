af9695d7bf65ccaf0e63f300c6c79db1ee2fbc82
petrec <petrec@coventry.ac.uk>
2019-11-07 11:37:39 +0000

    GREEN Added inexistent user check.
    * Pass 'invalid username' test.

--- modules/user/is-admin.js
+++ modules/user/is-admin.js
@@ -11,6 +11,7 @@ const isAdmin = async function(username) {
 	try {
 		const sql = 'SELECT id,is_admin FROM users where username=$1'
 		const { rows: [res] } = await this.db.query(sql, [username])
+		if(res === undefined) throw new Error(`username "${username}" not found`)
 		return res.is_admin
 	} catch(err) {
 		throw err
