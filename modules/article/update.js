f73107c630b159349c031c2a42887ac61559e8bc
petrec <petrec@coventry.ac.uk>
2019-11-08 18:19:54 +0000

    GREEN Added article ID validation.
    * Pass 'error if article ID is not numeric' test.

--- modules/article/update.js
+++ modules/article/update.js
@@ -1,6 +1,8 @@
 
 'use strict'
 
+const isId = require('../utils')
+
 /**
  * Updates an existing article submission.
  *
@@ -11,6 +13,7 @@
  * @returns {boolean} If the process was successful.
  */
 const update = async function(userId, articleId, newArticle) {
+	await isId(articleId)
 	const sql = 'UPDATE article SET (data, created_at, status) = ($2, now(), \'pending\') WHERE id=$1'
 	const {rowCount: updates} = await this.db.query(sql, [articleId, newArticle])
 	if(updates === 0) throw new Error(`article with ID "${articleId}" not found`)
