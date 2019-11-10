d11f766ca6ae74df7bcd331884badf1942c64e96
petrec <petrec@coventry.ac.uk>
2019-11-10 17:14:14 +0000

    REFACTOR Wrapped addOrUpdate() in try-catch block.

--- modules/rating/add-or-update.js
+++ modules/rating/add-or-update.js
@@ -4,16 +4,20 @@
 /**
  * Creates or updates an individual article rating.
  *
+ * @async
  * @param {number} userId - The ID of the user giving the rating.
  * @param {number} articleId - The ID of the rated article.
  * @param {number} value - The updated rating value on a scale of 1 to 5.
- * @async
  * @returns {boolean} If the process was successful.
  */
 const addOrUpdate = async function(userId, articleId, value) {
-	const sql = 'INSERT INTO rating (author_id, article_id, value) VALUES ($1, $2, $3)'
-	await this.db.query(sql, [userId, articleId, value])
-	return true
+	try {
+		const sql = 'INSERT INTO rating (author_id, article_id, value) VALUES ($1, $2, $3)'
+		await this.db.query(sql, [userId, articleId, value])
+		return true
+	} catch(err) {
+		throw err
+	}
 }
 
 module.exports = Rating => Rating.prototype.addOrUpdate = addOrUpdate
