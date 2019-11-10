a113e3339954f5d121d2ff86d774c918b31ff77d
petrec <petrec@coventry.ac.uk>
2019-11-10 17:25:54 +0000

    REFACTOR Use more explicit variable name for rating value.

--- modules/rating/add-or-update.js
+++ modules/rating/add-or-update.js
@@ -7,15 +7,15 @@
  * @async
  * @param {number} userId - The ID of the user giving the rating.
  * @param {number} articleId - The ID of the rated article.
- * @param {number} value - The updated rating value on a scale of 1 to 5.
+ * @param {number} rating - The updated rating value on a scale of 1 to 5.
  * @returns {boolean} If the process was successful.
  */
-const addOrUpdate = async function(userId, articleId, value) {
+const addOrUpdate = async function(userId, articleId, rating) {
 	try {
 		const sql = `INSERT INTO rating (author_id, article_id, value) VALUES ($1, $2, $3)
 			ON CONFLICT ON CONSTRAINT unique_rating DO UPDATE SET value=$3
 		`
-		await this.db.query(sql, [userId, articleId, value])
+		await this.db.query(sql, [userId, articleId, rating])
 		return true
 	} catch(err) {
 		throw err
