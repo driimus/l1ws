76cbc1ae6da74a6d410fb6fa8ea98aeb99681269
petrec <petrec@coventry.ac.uk>
2019-11-10 17:12:02 +0000

    GREEN Implemented article rating functionality.
    * Pass 'create a new article rating' test.

--- modules/rating/add-or-update.js
+++ modules/rating/add-or-update.js
@@ -11,7 +11,9 @@
  * @returns {boolean} If the process was successful.
  */
 const addOrUpdate = async function(userId, articleId, value) {
-	throw new Error('function not implemented')
+	const sql = 'INSERT INTO rating (author_id, article_id, value) VALUES ($1, $2, $3)'
+	await this.db.query(sql, [userId, articleId, value])
+	return true
 }
 
 module.exports = Rating => Rating.prototype.addOrUpdate = addOrUpdate
