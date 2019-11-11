e92cbb685247b36d70dffe7af85a78123aa6ef61
petrec <petrec@coventry.ac.uk>
2019-11-11 23:59:17 +0000

    REFACTOR Updated documentation and wrapped timer in try-catch.

--- modules/newsletter/get-timeleft.js
+++ modules/newsletter/get-timeleft.js
@@ -5,17 +5,23 @@
  * Determines the time left until sending out a newsletter.
  *
  * @async
+ * @param {object} today - Current time.
+ * @param {number} targetHour - Newsletter's delivery hour.
  *
  * @returns {number} Time left in ms until next occurrence of 8AM.
  */
 const getTimeLeft = async(today=new Date(), targetHour) => {
-	const now = new Date(today)
-	if (isNaN(now) === true) throw new Error(`not a valid date: ${today}`)
-	// Get time left until target hour.
-	let timeLeft = today.setHours(targetHour, 0, 0, 0) - now
-	// Use next day if current time is past targetHour.
-	if (timeLeft < 1) timeLeft = today.setDate(today.getDate() + 1) - now
-	return timeLeft
+	try {
+		const now = new Date(today)
+		if (isNaN(now) === true) throw new Error(`not a valid date: ${today}`)
+		// Get time left until target hour.
+		let timeLeft = today.setHours(targetHour, 0, 0, 0) - now
+		// Use next day if current time is past targetHour.
+		if (timeLeft < 1) timeLeft = today.setDate(today.getDate() + 1) - now
+		return timeLeft
+	} catch(err) {
+		throw err
+	}
 }
 
 module.exports = Newsletter => Newsletter.prototype.getTimeLeft = getTimeLeft
