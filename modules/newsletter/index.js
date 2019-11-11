9881fb437fb6f45646e4d386dc88300ead46b350
petrec <petrec@coventry.ac.uk>
2019-11-11 16:02:19 +0000

    RED Added nodemailer config for each environment.

--- modules/newsletter/index.js
+++ modules/newsletter/index.js
@@ -3,11 +3,14 @@
 
 const nodemailer = require('nodemailer')
 
+// Import nodemailer settings for current environment.
+const {transporter: settings} = require('../../config')[process.env.NODE_ENV]
+
 class Newsletter {
 
 	constructor() {
 		return (() => {
-			this.transporter = nodemailer.createTransport()
+			this.transporter = nodemailer.createTransport(settings)
 			return this
 		})()
 	}
