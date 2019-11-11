4a03f228efba353dc0c8ce7a92bb59deb82d92e8
petrec <petrec@coventry.ac.uk>
2019-11-11 10:07:22 +0000

    REFACTOR Use shorthand if-else operator when returning the rating.

--- modules/rating/get.js
+++ modules/rating/get.js
@@ -7,13 +7,12 @@
  * @async
  * @param {number} userId - The ID of the rating's author.
  * @param {number} articleId - The ID of the rated article.
- * @returns {number} Value of the individual rating.
+ * @returns {number} Value of the individual rating or NaN if not found.
  */
 const get = async function(userId, articleId) {
 	const sql = 'SELECT value FROM rating WHERE author_id=$1 AND article_id=$2'
 	const {rows: [rating]} = await this.db.query(sql, [userId, articleId])
-	if (rating === undefined) return NaN
-	return rating.value
+	return rating === undefined ? NaN : rating.value
 }
 
 module.exports = Rating => Rating.prototype.get = get
