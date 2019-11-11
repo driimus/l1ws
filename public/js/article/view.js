75f5a6a348d94dce3cf9fa7c7af409ca93ff77a8
petrec <petrec@coventry.ac.uk>
2019-11-11 12:55:01 +0000

    Implemented client-side rating functionality.
    * Added method to render individual and average rating values.
    * Added fetch method for submitting updated ratings.
    * Fixed star elements not changing color on hover/select.

--- public/js/article/view.js
+++ public/js/article/view.js
@@ -2,14 +2,45 @@
 'use strict'
 
 document.addEventListener('DOMContentLoaded', () => {
-	const data = document.getElementById('articleView')
-	setDate(data)
+	const data = document.getElementById('articleView'),
+		starContainer = document.querySelector('.stars'),
+		stars = Array.prototype.slice.call(starContainer.children)
+	starContainer.addEventListener('click', async e => rate(e, stars))
+	setDate(data.getAttribute('date'))
+	setRating(stars, data.getAttribute('rating'))
 })
 
 /**
  * Displays submission date in local time.
  */
-const setDate = (data) => {
-	const date = data.getAttribute('date')
-	document.getElementById('date').innerText = new Date(date).toLocaleString()
+const setDate = date => document.getElementById('date').innerText = new Date(date).toLocaleString()
+
+const starCount = 5
+
+/**
+ * Updates individual user rating.
+ */
+const setRating = (stars, rating) => {
+	const selected = document.querySelector('.is-selected')
+	// Reset current selection if there is one.
+	if (selected !== null) selected.classList.toggle('is-selected')
+	// Update selection to new rating.
+	if (Number.isInteger(rating))
+		stars[starCount - parseInt(rating)].classList.toggle('is-selected')
+}
+
+const rate = async(e, stars) => {
+	const newRating = starCount - stars.indexOf(e.target.parentElement),
+		res = await fetch('/article/{{id}}/rate', {
+			method: 'POST',
+			body: JSON.stringify( {value: newRating} ),
+			credentials: 'include',
+			mode: 'cors'
+		})
+	const {newAverage} = await res.json()
+	// Update client-side ratings.
+	setRating(stars, newRating)
+	document.getElementById('rating').innerText = `rated ${newAverage}/${starCount} on average`
+	// Disable unselecting the rating.
+	e.target.parentElement.classList.add('is-selected')
 }
