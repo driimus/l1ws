b84f265ac19429b434b6d420fbe874c5b9658ee1
petrec <petrec@coventry.ac.uk>
2019-11-11 14:21:39 +0000

    Documented client-side JS scripts that handle article ratings.

--- public/js/article/view.js
+++ public/js/article/view.js
@@ -1,11 +1,14 @@
 
 'use strict'
 
+let starCount
+
 document.addEventListener('DOMContentLoaded', () => {
 	const data = document.getElementById('articleView'),
 		starContainer = document.querySelector('.stars'),
 		stars = Array.prototype.slice.call(starContainer.children),
 		id = data.getAttribute('articleId')
+	starCount = stars.length
 	starContainer.addEventListener('click', async e => rate(e, stars, id))
 	setDate(data.getAttribute('date'))
 	setRating(stars, data.getAttribute('rating'))
@@ -13,13 +16,16 @@ document.addEventListener('DOMContentLoaded', () => {
 
 /**
  * Displays submission date in local time.
+ *
+ * @param {string} date - Timestamp with timezone.
  */
 const setDate = date => document.getElementById('date').innerText = new Date(date).toLocaleString()
 
-const starCount = 5
-
 /**
  * Updates individual user rating.
+ *
+ * @param {object} stars - HTML container of the 5 star rating.
+ * @param {number} rating - The up-to-date individual rating.
  */
 const setRating = (stars, rating) => {
 	const selected = document.querySelector('.is-selected')
@@ -30,6 +36,14 @@ const setRating = (stars, rating) => {
 		stars[starCount - parseInt(rating)].classList.toggle('is-selected')
 }
 
+/**
+ * Submits a new article rating and updates the average rating.
+ *
+ * @async
+ * @param {object} e - Event that triggered the call.
+ * @param {object} stars - HTML container of the 5 star rating.
+ * @param {number} articleId - The ID of the current article.
+ */
 const rate = async(e, stars, articleId) => {
 	const newRating = starCount - stars.indexOf(e.target.parentElement),
 		res = await fetch(`/article/${articleId}/rate`, {
