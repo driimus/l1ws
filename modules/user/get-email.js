09fbd007755cd2cc17dd04f84b354bffdd501640
petrec <petrec@coventry.ac.uk>
2019-11-11 16:43:49 +0000

    GREEN Throw an error if user does not exist.
    * Pass 'error if user does not exist' test.

--- modules/user/get-email.js
+++ modules/user/get-email.js
@@ -9,8 +9,9 @@
  * @returns {string} The account's email address.
  */
 const getEmail = async function(id) {
-	const sql = 'SELECT email FROM users WHERE id=$1'
+	const sql = 'SELECT id,email FROM users WHERE id=$1'
 	const {rows: [result]} = await this.db.query(sql, [id])
+	if (result === undefined) throw new Error(`user with ID "${id}" not found`)
 	return result.email
 }
 
