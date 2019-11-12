30792419d40040bc5ad035348a7130b31f13c42c
petrec <petrec@coventry.ac.uk>
2019-11-12 18:10:34 +0000

    REFACTOR Wrapped function in try-catch block.

--- modules/user/get-subscription.js
+++ modules/user/get-subscription.js
@@ -9,11 +9,15 @@
  * @returns {boolean} The account's subscription status.
  */
 const getSubscription = async function(id) {
-	const sql = 'SELECT is_subscribed FROM users WHERE id=$1'
-	const {rows: [user]} = await this.db.query(sql, [id])
-	// Every record must have a status.
-	if (user === undefined) throw new Error(`user with ID "${id}" not found`)
-	return user.is_subscribed
+	try {
+		const sql = 'SELECT is_subscribed FROM users WHERE id=$1'
+		const {rows: [user]} = await this.db.query(sql, [id])
+		// Every record must have a status.
+		if (user === undefined) throw new Error(`user with ID "${id}" not found`)
+		return user.is_subscribed
+	} catch(err) {
+		throw err
+	}
 }
 
 module.exports = User => User.prototype.getSubscription = getSubscription
