1700548741b6e36f33151d371dc6ded7b4ef29e0
petrec <petrec@coventry.ac.uk>
2019-11-12 18:25:39 +0000

    GREEN Set user email for new accounts if it's valid.

--- modules/user/register.js
+++ modules/user/register.js
@@ -4,6 +4,7 @@
 const bcrypt = require('bcrypt-promise')
 
 const {saltRounds} = require('../../config')[process.env.NODE_ENV]
+const {isEmail} = require('../utils')
 
 /**
  * Creates a new user with the given credentials.
@@ -13,8 +14,9 @@ const {saltRounds} = require('../../config')[process.env.NODE_ENV]
  * @async
  * @returns {boolean} Whether the operation was successful.
  */
-const register = async function(username, password) {
+const register = async function(username, password, email) {
 	try {
+		email = isEmail(email) === true ? email : null
 		if(username.length === 0) throw new Error('missing username')
 		if(password.length === 0) throw new Error('missing password')
 		// Check that the username is not taken.
@@ -23,8 +25,8 @@ const register = async function(username, password) {
 		if(exists !== 0) throw new Error(`username "${username}" already in use`)
 		// Save username and encrypted password.
 		password = await bcrypt.hash(password, saltRounds)
-		sql = 'INSERT INTO users(username, password) VALUES($1, $2)'
-		await this.db.query(sql, [username, password])
+		sql = 'INSERT INTO users(username, password, email) VALUES($1, $2, $3)'
+		await this.db.query(sql, [username, password, email])
 		return true
 	} catch(err) {
 		throw err
