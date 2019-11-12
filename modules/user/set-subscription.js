269a58bc28bc0de9707cf42496c981601f9e91bf
petrec <petrec@coventry.ac.uk>
2019-11-12 17:24:08 +0000

    REFACTOR Added try-catch block and meaningful variable name.

--- modules/user/set-subscription.js
+++ modules/user/set-subscription.js
@@ -9,10 +9,14 @@
  * @returns {boolean} Whether the status was successfully changed.
  */
 const setSubscription = async function(id, newStatus) {
-	const sql = 'UPDATE users SET is_subscribed=$2 WHERE id=$1'
-	const {rowCount} = await this.db.query(sql, [id, newStatus])
-	if(rowCount === 0) throw new Error(`user with ID "${id}" not found`)
-	return true
+	try {
+		const sql = 'UPDATE users SET is_subscribed=$2 WHERE id=$1'
+		const {rowCount: updates} = await this.db.query(sql, [id, newStatus])
+		if (updates === 0) throw new Error(`user with ID "${id}" not found`)
+		return true
+	} catch(err) {
+		throw err
+	}
 }
 
 module.exports = User => User.prototype.setSubscription = setSubscription
