b289b596b75d1f54d4ca5d3300f302377eeb6054
petrec <petrec@coventry.ac.uk>
2019-11-12 17:55:00 +0000

    GREEN Implemented newsletter subscribed status getter for users.
    * Pass 'get subscribed user status' test.

--- modules/user/get-subscription.js
+++ modules/user/get-subscription.js
@@ -2,7 +2,9 @@
 'use strict'
 
 const getSubscription = async function(id) {
-	throw new Error('fn not implemented')
+	const sql = 'SELECT is_subscribed FROM users WHERE id=$1'
+	const {rows: [{is_subscribed: subscribed}]} = await this.db.query(sql, [id])
+	return subscribed
 }
 
 module.exports = User => User.prototype.getSubscription = getSubscription
