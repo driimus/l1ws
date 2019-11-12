5c9082042ab7edc7e11e4e3dbac6cda732187ddc
petrec <petrec@coventry.ac.uk>
2019-11-12 18:35:45 +0000

    GREEN Implemented newsletter mailing list getter.
    * Pass 'get list of newsletter recipients' test.

--- modules/user/get-mailinglist.js
+++ modules/user/get-mailinglist.js
@@ -9,7 +9,9 @@
  * @returns {string} The account's email address.
  */
 const getMailingList = async function() {
-	throw new Error('fn not implemented')
+	const sql = 'SELECT email FROM users WHERE is_subscribed'
+	const {rows: recipients} = await this.db.query(sql)
+	return recipients.map(user => user.email)
 }
 
 module.exports = User => User.prototype.getMailingList = getMailingList
