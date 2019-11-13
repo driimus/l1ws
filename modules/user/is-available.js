46c06016db198be667274e0be6b7cfbf0648edc1
petrec <petrec@coventry.ac.uk>
2019-11-13 14:36:11 +0000

    GREEN Added validation helpers to FIELDS schema.

--- modules/user/is-available.js
+++ modules/user/is-available.js
@@ -1,9 +1,17 @@
 
 'use strict'
 
-// User arrtibutes for which availability checks are enabled.
-const FIELDS = ['username','email']
+const {isEmail} = require('../utils')
 
+// User arrtibutes for which availability checks are enabled.
+const FIELDS = {
+	username: value => {
+		if(typeof value !== 'string' || value.length === 0) throw new Error('missing username')
+	},
+	email: value => {
+		if(isEmail(value) === false) throw new Error('invalid email address format')
+	}
+}
 /**
  * Checks whether the value is available for a given user attribute.
  *
@@ -12,7 +20,9 @@ const FIELDS = ['username','email']
  * @param {string} value - Value to be searched for.
  */
 const isAvailable = async function(field, value) {
-	if(FIELDS.includes(field) === false) throw new Error(`invalid field "${field}"`)
+	if(Object.keys(FIELDS).includes(field) === false) throw new Error(`invalid field "${field}"`)
+	// if(value.length === 0) throw new Error(`missing ${field}`)
+	FIELDS[field](value)
 	// Check that the username is not taken.
 	const sql = `SELECT id FROM users WHERE ${field}=$1 LIMIT 1`
 	const { rows: {length: exists} } = await this.db.query(sql, [value])
