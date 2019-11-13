7ffc9c433f512572f7279dbc4154a4bec034e746
petrec <petrec@coventry.ac.uk>
2019-11-13 22:49:02 +0000

    REFACTOR Use helper function for input re-formatting.

--- modules/article/find.js
+++ modules/article/find.js
@@ -1,15 +1,24 @@
 
 'use strict'
 
+const formatted = {
+	keyphrase: phrase => phrase.split(' ').join(' & '),
+	showHidden: flag => flag === false ? 'AND status=\'approved\'' : ''
+}
+
 const find = async function(keyphrase, showHidden=false) {
-	keyphrase = keyphrase.split(' ').join(' & ')
-	const sql = `SELECT * FROM article
-		WHERE searchable_indices @@ to_tsquery($1)
-		${showHidden === false ? 'AND status=\'approved\'' : ''}
-	`
-	const {rows: articles} = await this.db.query(sql, [keyphrase])
-	if(articles.length === 0) throw new Error('the search generated no results')
-	return articles
+	try {
+		keyphrase = formatted.keyphrase(keyphrase)
+		showHidden = formatted.showHidden(showHidden)
+		const sql = `SELECT * FROM article
+			WHERE searchable_indices @@ to_tsquery($1) ${showHidden}
+		`
+		const {rows: articles} = await this.db.query(sql, [keyphrase])
+		if(articles.length === 0) throw new Error('the search generated no results')
+		return articles
+	} catch(err) {
+		throw err
+	}
 }
 
 module.exports = Article => Article.prototype.find = find
