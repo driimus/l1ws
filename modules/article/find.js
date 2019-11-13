dfb436d526aaf6b165b20978a68a7f31ffa53cce
petrec <petrec@coventry.ac.uk>
2019-11-13 23:04:52 +0000

    REFACTOR Added documentation and extra comments to article search.

--- modules/article/find.js
+++ modules/article/find.js
@@ -1,7 +1,7 @@
 
 'use strict'
 
-const formatted = {
+const parsed = {
 	keyphrase: phrase => {
 		if (typeof phrase !== 'string' || phrase.length === 0) throw new Error('missing search keyphrase')
 		// Format the keyphrase into a valid Postgres value.
@@ -10,10 +10,20 @@ const formatted = {
 	showHidden: flag => flag === false ? 'AND status=\'approved\'' : ''
 }
 
+/**
+ * Retrieves all the articles with data containing the keyphrase.
+ *
+ * @async
+ * @param {string} keyphrase - Phrase containing all the desired words.
+ * @param {boolean} showHidden - Flag for filtering articles by status.
+ * @returns {object[]} Articles with matching content.
+ */
 const find = async function(keyphrase, showHidden=false) {
 	try {
-		keyphrase = formatted.keyphrase(keyphrase)
-		showHidden = formatted.showHidden(showHidden)
+		// Parse and validate arguments for query compatibility.
+		keyphrase = parsed.keyphrase(keyphrase)
+		showHidden = parsed.showHidden(showHidden)
+		// Search indexed list of article words.
 		const sql = `SELECT * FROM article
 			WHERE searchable_indices @@ to_tsquery($1) ${showHidden}
 		`
