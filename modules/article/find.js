73ba33f66b7b4a2f94be418f4575b22c409a998d
petrec <petrec@coventry.ac.uk>
2019-11-13 19:06:45 +0000

    GREEN Implemented full-text search functionality for articles.
    * Pass 'find article by headline keyword' test.
    * Pass 'find article by summary keyword' test.
    * Pass 'find article by content keyword' test.

--- modules/article/find.js
+++ modules/article/find.js
@@ -2,7 +2,9 @@
 'use strict'
 
 const find = async function(keyphrase, showHidden=false) {
-	throw new Error('fn not implemented')
+	const sql = 'SELECT * FROM article WHERE searchable_indices @@ to_tsquery($1)'
+	const {rows: articles} = await this.db.query(sql, [keyphrase])
+	return articles
 }
 
 module.exports = Article => Article.prototype.find = find
--- modules/article/index.js
+++ modules/article/index.js
@@ -8,11 +8,31 @@ const schema = `CREATE TABLE IF NOT EXISTS article (
 	id SERIAL PRIMARY KEY,
 	author_id INTEGER,
 	data JSON NOT NULL,
-	created_at TIMESTAMPTZ DEFAULT now())`,
+	created_at TIMESTAMPTZ DEFAULT now());
+	`,
 	// Article schema upgrades.
 	upgrade = `ALTER TABLE article
+	ADD COLUMN IF NOT EXISTS searchable_indices TSVECTOR,
 	ADD COLUMN IF NOT EXISTS status TEXT DEFAULT 'pending'
-		CHECK (status in ('pending','approved','rejected'))`
+		CHECK (status in ('pending','approved','rejected'));
+	CREATE INDEX IF NOT EXISTS searchable_idx ON article USING GIN(searchable_indices);
+	`,
+	trigger = schema => `CREATE OR REPLACE FUNCTION ${schema}.indices_update_trigger()
+	RETURNS TRIGGER AS $$
+	BEGIN
+		NEW.searchable_indices := to_tsvector('english', NEW.data);
+		RETURN NEW;
+	END;
+	$$ LANGUAGE plpgsql;
+	DROP TRIGGER IF EXISTS tsvectorupdate ON article;
+	CREATE TRIGGER tsvectorupdate BEFORE INSERT OR UPDATE
+	ON article FOR EACH ROW EXECUTE PROCEDURE ${schema}.indices_update_trigger();`
+
+const scope = {
+	development: 'public',
+	production: 'public',
+	test: 'pg_temp'
+}
 
 /** Class representing an article. */
 class Article {
@@ -25,6 +45,7 @@ class Article {
 			this.db = new db()
 			await this.db.query(schema)
 			await this.db.query(upgrade)
+			await this.db.query(trigger(scope[process.env.NODE_ENV]))
 			return this
 		})()
 	}
