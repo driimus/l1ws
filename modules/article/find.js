cb9cae2959df9d3757b5228a164a6c11a741357a
petrec <petrec@coventry.ac.uk>
2019-11-13 22:28:41 +0000

    GREEN Throw an error for searches with no results.
    * Pass 'error if no search results' test.

--- modules/article/find.js
+++ modules/article/find.js
@@ -3,8 +3,12 @@
 
 const find = async function(keyphrase, showHidden=false) {
 	keyphrase = keyphrase.split(' ').join(' & ')
-	const sql = 'SELECT * FROM article WHERE searchable_indices @@ to_tsquery($1)'
+	const sql = `SELECT * FROM article
+		WHERE searchable_indices @@ to_tsquery($1)
+		${showHidden === false ? 'AND status=\'approved\'' : ''}
+	`
 	const {rows: articles} = await this.db.query(sql, [keyphrase])
+	if(articles.length === 0) throw new Error('the search generated no results')
 	return articles
 }
 
