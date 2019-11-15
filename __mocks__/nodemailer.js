f42906bdcaf2ed4a71ba9d3855f645ac867951a1
petrec <petrec@coventry.ac.uk>
2019-11-15 12:54:06 +0000

    Implemented mail object parser for Nodemailer mock.

--- __mocks__/nodemailer.js
+++ __mocks__/nodemailer.js
@@ -3,6 +3,8 @@
 
 const nodemailer = jest.requireActual('nodemailer')
 
+const addressparser = require('../node_modules/nodemailer/lib/addressparser')
+
 const mockmailer = jest.genMockFromModule('nodemailer')
 
 let transport, sentMail = []
@@ -12,8 +14,66 @@ const __resetMock = () => {
 }
 
 function createTransport(options) {
+	// Use the real module's transporter.
 	transport = nodemailer.createTransport(options)
 	return {sendMail}
+}
+
+// Use modified versions of the real module's.
+const Mail = {
+
+	/**
+	 * Normalizes an email address
+	 *
+	 * @param {Array} address An array of address objects
+	 * @return {String} address string
+	 */
+	_normalizeAddress: address => {
+		address = (address || '').toString().trim()
+
+		const lastAt = address.lastIndexOf('@')
+		if (lastAt < 0) {
+			// Bare username
+			return address
+		}
+		const user = address.substr(0, lastAt)
+		const domain = address.substr(lastAt + 1)
+
+		// Usernames are not touched and are kept as is even if these include unicode
+		return `${user }@${ domain.toLowerCase()}`
+	},
+	/**
+	 * Parses addresses. Takes in a single address or an array or an
+	 * array of address arrays (eg. To: [[first group], [second group],...])
+	 *
+	 * @param {Mixed} addresses Addresses to be parsed
+	 * @return {Array} An array of address objects
+	 */
+	_parseAddresses: addresses => [].concat.apply([],
+		[].concat(addresses).map(address => {
+			// eslint-disable-line prefer-spread
+			if (address && address.address) {
+				address.address = Mail._normalizeAddress(address.address)
+				address.name = address.name || ''
+				return [address]
+			}
+			return addressparser(address)
+		})
+	),
+
+	_parseContent: data => {
+		['html', 'text', 'watchHtml', 'amp'].forEach(key => {
+			if (data[key] && data[key].content) {
+				if (typeof data[key].content === 'string') {
+					data[key] = data[key].content
+				} else if (Buffer.isBuffer(data[key].content)) {
+					data[key] = data[key].content.toString()
+				}
+			}
+		})
+		return data
+	}
+
 }
 
 const sendMail = (email, callback) => {
@@ -21,6 +81,10 @@ const sendMail = (email, callback) => {
 	const isPromise = !callback && typeof Promise === 'function'
 
 	try {
+		// Parse the email object.
+		email.to = Mail._parseAddresses(email.to)
+		email = Mail._parseContent(email)
+		// Cache the sent email.
 		sentMail.push(email)
 		// return success
 		return isPromise ? Promise.resolve('Mock mail sent') : callback(null, 'Mock mail sent')
