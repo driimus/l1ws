9a293878787b925b4d1c658ac7dddda39311aa42
petrec <petrec@coventry.ac.uk>
2019-11-17 17:26:53 +0000

    REFACTOR Wrapped avatar getter in try-catch block.

--- modules/user/get-avatar.js
+++ modules/user/get-avatar.js
@@ -9,10 +9,14 @@
  * @returns {string} Path to the avatar image.
  */
 async function getAvatar(id) {
-	const sql = 'SELECT avatar FROM users WHERE id=$1'
-	const {rows: [user]} = await this.db.query(sql, [id])
-	if (user === undefined) throw new Error(`user with ID "${id}" not found`)
-	return user.avatar
+	try {
+		const sql = 'SELECT avatar FROM users WHERE id=$1'
+		const {rows: [user]} = await this.db.query(sql, [id])
+		if (user === undefined) throw new Error(`user with ID "${id}" not found`)
+		return user.avatar
+	} catch(err) {
+		throw err
+	}
 }
 
 module.exports = User => User.prototype.getAvatar = getAvatar
