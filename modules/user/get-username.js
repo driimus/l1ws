0b4be9333467a3a09c1626a865310da91577f8d7
petrec <petrec@coventry.ac.uk>
2019-11-20 18:09:54 +0000

    REFACTOR Wrapped username getter in try-catch block.

--- modules/user/get-username.js
+++ modules/user/get-username.js
@@ -2,10 +2,14 @@
 'use strict'
 
 const getUsername = async function(id) {
-	const sql = 'SELECT username FROM users WHERE id=$1'
-	const {rows: [user]} = await this.db.query(sql, [id])
-	if (user === undefined) throw new Error(`user with ID "${id}" not found`)
-	return user.username
+	try {
+		const sql = 'SELECT username FROM users WHERE id=$1'
+		const {rows: [user]} = await this.db.query(sql, [id])
+		if (user === undefined) throw new Error(`user with ID "${id}" not found`)
+		return user.username
+	} catch(err) {
+		throw err
+	}
 }
 
 module.exports = User => User.prototype.getUsername = getUsername
