3738be1742546025d168c97989ad8cb4f20a34ee
petrec <petrec@coventry.ac.uk>
2019-11-20 18:12:27 +0000

    GREEN Implemented id validation in username getter.
    * Pass 'error if invalid user id' test.

--- modules/user/get-username.js
+++ modules/user/get-username.js
@@ -1,8 +1,11 @@
 
 'use strict'
 
+const {isId} = require('../utils')
+
 const getUsername = async function(id) {
 	try {
+		id = await isId(id, 'user')
 		const sql = 'SELECT username FROM users WHERE id=$1'
 		const {rows: [user]} = await this.db.query(sql, [id])
 		if (user === undefined) throw new Error(`user with ID "${id}" not found`)
