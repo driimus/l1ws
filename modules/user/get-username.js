00a776387021249501c2d6ec9d8cc23685c703de
petrec <petrec@coventry.ac.uk>
2019-11-20 18:05:52 +0000

    GREEN Implemented username getter for accounts.
    * Pass 'get valid account username' test.

---
+++ modules/user/get-username.js
@@ -0,0 +1,10 @@
+
+'use strict'
+
+const getUsername = async function(id) {
+	const sql = 'SELECT id,username FROM users WHERE id=$1'
+	const {rows: [user]} = await this.db.query(sql, [id])
+	return user.username
+}
+
+module.exports = User => User.prototype.getUsername = getUsername
