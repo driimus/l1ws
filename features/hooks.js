47ffd637f12f1d65fbdb19cb6d08d3b514043ba4
petrec <petrec@coventry.ac.uk>
2019-11-20 16:48:23 +0000

    FIX Acceptance tests using isolated database schemas.

--- features/hooks.js
+++ features/hooks.js
@@ -1,10 +1,19 @@
 
 'use strict'
 
-const {AfterAll} = require('cucumber')
+const {BeforeAll, AfterAll} = require('cucumber')
 const scope = require('./support/scope')
 
+const db = require('../db')
+
+BeforeAll(async() => {
+	const pool = new db()
+	await pool.query('ALTER ROLE petest SET search_path = public')
+})
+
 AfterAll(async() => {
+	const pool = new db()
+	await pool.query('ALTER ROLE petest SET search_path = pg_temp')
 	if (scope.browser !== undefined) await scope.browser.close()
 	// Stop the test server.
 	scope.app.close(() => console.log('Shutting down server...'))
