b07eaca92ad8c4a0585762835c4db73400d54243
petrec <petrec@coventry.ac.uk>
2019-11-22 16:18:34 +0000

    FIX Keep server running between scenarios with a truncated database.

--- features/hooks.js
+++ features/hooks.js
@@ -1,14 +1,36 @@
 
 'use strict'
 
-const {BeforeAll, AfterAll} = require('cucumber')
+const {BeforeAll, After, AfterAll} = require('cucumber')
 const scope = require('./support/scope')
 
+const app = require('../')
 const db = require('../db')
+
+const defaultPort = 8080
 
 BeforeAll(async() => {
 	const pool = new db()
 	await pool.query('ALTER ROLE petest SET search_path = public')
+
+	// Run the server on a custom port.
+	scope.port = process.env.PORT || defaultPort
+	scope.app = app.listen(scope.port)
+})
+
+After(async() => {
+	const pool = new db(), dbname = pool.options.database
+	// await pool.query('TRUNCATE TABLE users')
+
+	let session = scope.context.currentPage
+	// Exit if there is no session.
+	if (scope.browser === undefined || session === undefined) return
+	const cookies = await session.cookies()
+	// Clear app cookies to reset koa session.
+	if (cookies !== undefined && cookies.length !== 0)
+		await session.deleteCookie(...cookies)
+	await session.close()
+	session = null
 })
 
 AfterAll(async() => {
--- features/world.js
+++ features/world.js
@@ -6,10 +6,6 @@ const puppeteer = require('puppeteer')
 
 const scope = require('./support/scope')
 
-// Run the server on a custom port.
-const defaultPort = 8080
-const port = process.env.PORT || defaultPort
-
 const admin = {
 	username: 'admin',
 	password: 'secretpass',
@@ -20,16 +16,13 @@ process.env.ADMIN_USER = admin.username
 process.env.ADMIN_PASS = admin.password
 process.env.ADMIN_EMAIL = admin.email
 
-const app = require('../')
-
 const World = function() {
-	scope.host = `http://localhost:${port}`
+	scope.host = `http://localhost:${scope.port}`
 	scope.driver = puppeteer
 	scope.context = {
 		accounts: [],
 		admin
 	}
-	scope.app = app.listen(port)
 }
 
 setWorldConstructor(World)
